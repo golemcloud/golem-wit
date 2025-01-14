e40914096c9554bdfe1a01226b2184fd105d9e31
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2025-01-14 14:31:43 +0100

    Exposing WASM_RPC_WIT and WASI_POLL_WIT as constants

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
 // Empty library for packaging WIT and adapter modules
+
+pub const WASM_RPC_WIT: &str = include_str!("../wit/deps/wasm-rpc/wasm-rpc.wit");
+pub const WASI_POLL_WIT: &str = include_str!("../wit/deps/io/poll.wit");
