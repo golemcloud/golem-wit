044dba05b1f883a821cc767e151456dddb3f455f
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2025-02-06 18:52:37 +0100

    include wall clock wit

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@
 
 pub const WASM_RPC_WIT: &str = include_str!("../wit/deps/wasm-rpc/wasm-rpc.wit");
 pub const WASI_POLL_WIT: &str = include_str!("../wit/deps/io/poll.wit");
+pub const WASI_WALL_CLOCKS_WIT: &str = include_str!("../wit/deps/clocks/wall-clock.wit");
