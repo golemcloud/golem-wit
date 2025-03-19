36403af2b3fd1f41c198cc141627709828204f66
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2025-03-19 10:51:16 +0100

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 // Empty library for packaging WIT and adapter modules
 
-pub const WASM_RPC_WIT: &str = include_str!("../wit/deps/wasm-rpc/wasm-rpc.wit");
+pub const WASM_RPC_WIT: &str = include_str!("../wit/deps/golem-rpc/wasm-rpc.wit");
 pub const WASI_POLL_WIT: &str = include_str!("../wit/deps/io/poll.wit");
 pub const WASI_WALL_CLOCKS_WIT: &str = include_str!("../wit/deps/clocks/wall-clock.wit");
